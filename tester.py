d26703333546ebd8d04085d6d248e23a4f2a3162
nathanaelrogers <nathanael.rogers@outlook.com>
2024-11-27 23:07:16 +0000

    tests are a mess... for now

--- tester.py
+++ tester.py
@@ -2,16 +2,28 @@ import subprocess
 import os
 
 class TestADC:
-	def test_standard_addition(self):
+	def test_addition_manually(self):
+		commands = [
+			'reg_A = 0x20',
+			':run',
+			'execute(ADC_IMM(0x14))',
+			':run',
+			'reg_A',
+			':run',
+			':quit'
+		]
+
+	def test_addition_auto(self):
+		commands = [
+			':bin 0x0200, tests/ADC/imm.bin',
+			'main()',
+			':run',
+			':quit'
+		]
+
 		# Write SAIL interpreter commands to file
 		with open('commands.txt', 'w') as file:
-			file.write('access(reg(A)) = 0x20\n')
-			file.write(':run\n')
-			file.write('execute(ADC_IMM(0x14))\n')
-			file.write(':run\n')
-			file.write('access(reg(A))\n')
-			file.write(':run\n')
-			file.write(':quit')
+			file.write('\n'.join(commands))
 
 		# Run SAIL intepreter using commands
 		result = subprocess.run(['sail', '-is', 'commands.txt','main.sail'], capture_output=True)
@@ -21,4 +33,7 @@ class TestADC:
 
 		# Do something with the results
 		output = result.stdout.decode('UTF-8')
-		assert 'Result = 0x34' in output
+		expected = [
+		]
+		for item in expected:
+			assert item in output
