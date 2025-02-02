75a197b0cf1c86e92a5a62063c46e994b5987243
nathanaelrogers <nathanael.rogers@outlook.com>
2025-02-02 01:20:47 +0000

    done for now, DISCOVERED BUG WITH LOOPING JUMP IN FUNCTIONAL TEST ON Y=0x67

--- fake6502/driver.c
+++ fake6502/driver.c
@@ -16,22 +16,40 @@ void write6502(uint16_t address, uint8_t value) {
 	return;
 }
 
-int main() {
-	// Initialise the main memory
-	main_mem = (memory) calloc((0b1 << 16), sizeof(uint8_t));
-
+int functional_test_partone() {
 	// Open the code file
-	FILE *code = fopen("code.bin", "rb");
+	FILE *code = fopen("test/sourcefiles/functional/6502_functional_test.bin", "rb");
 	if (!code) {
 		fprintf(stderr, "could not open code.bin!\n");
 		return 1;
 	}
 	// Read the code into memory
-	fread(main_mem, sizeof(uint8_t), (0b1 << 16), code);
+	fread(main_mem + 0x0A, sizeof(uint8_t), (0b1 << 16), code);
 
-	while(1) {
+	// run the code
+	reset6502();
+	pc = 0x0400;
+	status = 0x06;
+	while (1) {
+		if (pc == 0x0446) {
+			printf("loop started, Y: %x\n", y);
+		}
+
 		step6502();
-	}
 
-	return 0;
+		if (pc == 0x0581) {
+			printf("DONE-ZO\n");
+			printf("instructions done: %d\n", instructions);
+			printf("cycles done: %d\n", clockticks6502);
+			printf("reached PC: %x\n", pc);
+			return 0;
+		}
+	}
+}
+
+int main() {
+	// Initialise the main memory
+	main_mem = (memory) calloc((0b1 << 16), sizeof(uint8_t));
+
+	return functional_test_partone();
 }
