13cb056f4554261514f9124ff971c53c8ac971e1
nathanaelrogers <nathanael.rogers@outlook.com>
2025-02-04 20:49:55 +0000

    enabled RAM integrity check for large functional test

--- test/test_bcd.py
+++ test/test_bcd.py
@@ -6,7 +6,7 @@ class TestBCD:
 		results = util.create(start_binary=0x0000, source_file='test/sourcefiles/BCD/valid-only.s', view_memory=[0x08FF], compile_c_target=True)
 		print(results)
 
-		result = re.search(r'0x08ff: (0x\d\d)', results)
+		result = re.search(r'0x08ff: (0x\w\w)', results)
 		assert result
 		assert int(result.group(1), 16) == 0x00
 
@@ -14,6 +14,6 @@ class TestBCD:
 		results = util.create(start_binary=0x0000, source_file='test/sourcefiles/BCD/full.s', view_memory=[0x08FF], compile_c_target=True)
 		print(results)
 
-		result = re.search(r'0x08ff: (0x\d\d)', results)
+		result = re.search(r'0x08ff: (0x\w\w)', results)
 		assert result
 		assert int(result.group(1), 16) == 0x00
--- test/test_functional.py
+++ test/test_functional.py
@@ -3,9 +3,9 @@ import re
 
 class TestFunctional:
 	def test_functional(self):
-		results = util.create(start_binary=0x000a, overrided_start_pc=0x0400, source_file='test/sourcefiles/functional/6502_functional_test.bin', generate_binary=False, compile_c_target=True, enable_print_dump=False, enable_print_at_interval=True, enable_break_at_trap=True)
+		results = util.create(start_binary=0x000a, overrided_start_pc=0x0400, source_file='test/sourcefiles/functional/6502_functional_test.bin', generate_binary=False, compile_c_target=True, enable_break_at_trap=True)
 		print(results)
 
-		result = re.search(r'trapped PC: (0x\d{4})', results)
+		result = re.search(r'trapped PC: (0x\w{4})', results)
 		assert result
-		assert int(result.group(1), 16) == 0x3469
+		assert int(result.group(1), 16) == 0x34D8
