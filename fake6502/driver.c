9ed21c69d4b6b323fce42b9a49d551a9d65ec5ff
nathanaelrogers <nathanael.rogers@outlook.com>
2025-03-13 22:06:32 +0000

    minor renaming

--- fake6502/driver.c
+++ fake6502/driver.c
@@ -1,4 +1,4 @@
-#include "fake6502-new.h"
+#include "fake6502.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,14 +23,16 @@ int functional_test() {
 		fprintf(stderr, "could not open 6502_functional_test.bin!\n");
 		return 1;
 	}
+
 	// Read the code into memory
 	fread(main_mem + 0x0A, sizeof(unsigned char), (0b1 << 16), code);
 
-	// run the code
+	// initialise the processor
 	reset6502();
 	pc = 0x0400;
 	status = 0x06;
 
+	// run
 	exec6502_until_trap();
 	printf("cycles: %d, instructions: %d, pc: %x\n", clockticks6502, instructions, pc);
 	return 0;
@@ -43,14 +45,15 @@ int functional_test_no_bcd() {
 		fprintf(stderr, "could not open 6502_functional_test_no_bcd.bin!\n");
 		return 1;
 	}
+
 	// Read the code into memory
 	fread(main_mem + 0x0A, sizeof(unsigned char), (0b1 << 16), code);
 
-	// run the code
+	// initialise the processor
 	reset6502();
 	pc = 0x0400;
-	status = 0x06;
 
+	// run
 	exec6502_until_trap();
 	printf("cycles: %d, instructions: %d, pc: %x\n", clockticks6502, instructions, pc);
 	return 0;
@@ -63,15 +66,17 @@ int bcd_test() {
 		fprintf(stderr, "could not open valid-only.bin!\n");
 		return 1;
 	}
+
 	// Read the code into memory
 	fread(main_mem, sizeof(unsigned char), (0b1 << 16), code);
 
-	// run the code
+	// initialise the processor
 	reset6502();
 	pc = 0x0000;
-	status = 0x06;
 
+	// run
 	exec6502_until_trap();
+	printf("result in address 0x08FF: %x\n", main_mem[0x08FF]);
 	printf("cycles: %d, instructions: %d, pc: %x\n", clockticks6502, instructions, pc);
 	return 0;
 }
@@ -80,5 +85,5 @@ int main() {
 	// Initialise the main memory
 	main_mem = (memory) calloc((0b1 << 16), sizeof(unsigned char));
 
-	return functional_test();
+	return bcd_test();
 }
--- test/test_bcd.py
+++ test/test_bcd.py
@@ -2,18 +2,16 @@ from util import load_test, check_acc
 import re
 
 class TestBCD:
-	def test_clark_valid_only(self):
+	def test_bcd_valid_only(self):
 		results = load_test(start_binary=0x0000, source_file='test/sourcefiles/BCD/valid-only.s', view_memory=[0x08FF], compile_c_target=True)
 		print(results)
 
 		result = re.search(r'0x08ff: (0x\w\w)', results)
-		assert result
-		assert check_acc(result, 0x00)
+		assert result and check_acc(result, 0x00)
 
-	def test_clark_full(self):
+	def test_bcd_full(self):
 		results = load_test(start_binary=0x0000, source_file='test/sourcefiles/BCD/full.s', view_memory=[0x08FF], compile_c_target=True)
 		print(results)
 
 		result = re.search(r'0x08ff: (0x\w\w)', results)
-		assert result
-		assert int(result.group(1), 16) == 0x00
+		assert result and check_acc(result, 0x00)
